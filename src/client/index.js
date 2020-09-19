5901907e8fb4863d24b889a96cf22a148e9961ec
btsixer <60943928+btsixer@users.noreply.github.com>
2020-09-19 12:40:00 -0600

    Add files via upload

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,19 @@
 import {
     //main function from client.js
-    init
+    handleSubmit
 } from './js/client'
   
 import './styles/style.scss'
   
   
-// Call init on DOMContentLoaded event.
-window.addEventListener('DOMContentLoaded', init);
+// Call travelCard event listener after DOMContentLoaded event.
+window.addEventListener('DOMContentLoaded', _ => {
+    const travelCard = document.getElementById('input-submit');
+    travelCard.addEventListener('click', handleSubmit);
+});
   
   
 export {
     //main function from client.js
-    init
+    handleSubmit
 }
